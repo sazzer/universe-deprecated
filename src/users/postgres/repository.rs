f78b9315c04a0108d0a5272243739012d3e24b7e
Graham Cox <graham@grahamcox.co.uk>
2019-12-30 10:50:19 +0000

    Repository methods to get users by ID and Email

--- src/users/postgres/repository.rs
+++ src/users/postgres/repository.rs
@@ -1,8 +1,9 @@
 use crate::database::Database;
 use crate::entity::Identity;
 use crate::users::repository::UserRepository;
-use crate::users::{UserData, UserEntity, Username};
+use crate::users::{UserData, UserEntity, UserID, Username};
 use log::warn;
+use postgres::types::ToSql;
 use std::sync::Arc;
 
 pub struct PostgresUserRepository {
@@ -13,6 +14,30 @@ pub struct PostgresUserRepository {
 impl PostgresUserRepository {
     pub fn new(database: Arc<dyn Database>) -> Self {
         Self { database }
+    }
+
+    fn get_single_user(&self, query: &str, params: &[&(dyn ToSql + Sync)]) -> Option<UserEntity> {
+        let row = self.database.client().ok()?.query_one(query, params);
+
+        match row {
+            Err(e) => {
+                warn!("Failed to get user: {}", e);
+                None
+            }
+            Ok(row) => Some(row.into()),
+        }
+    }
+}
+
+impl UserRepository for PostgresUserRepository {
+    fn get_user_by_id(&self, user_id: UserID) -> Option<UserEntity> {
+        self.get_single_user("SELECT * FROM users WHERE user_id = $1", &[&user_id])
+    }
+    fn get_user_by_email(&self, email: String) -> Option<UserEntity> {
+        self.get_single_user("SELECT * FROM users WHERE email = $1", &[&email])
+    }
+    fn get_user_by_username(&self, username: Username) -> Option<UserEntity> {
+        self.get_single_user("SELECT * FROM users WHERE username = $1", &[&username])
     }
 }
 
@@ -35,30 +60,30 @@ impl From<postgres::Row> for UserEntity {
     }
 }
 
-impl UserRepository for PostgresUserRepository {
-    fn get_user_by_username(&self, username: Username) -> Option<UserEntity> {
-        let row = self
-            .database
-            .client()
-            .ok()?
-            .query_one("SELECT * FROM users WHERE username = $1", &[&username]);
-
-        match row {
-            Err(e) => {
-                warn!("Failed to get user: {}", e);
-                None
-            }
-            Ok(row) => Some(row.into()),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::database::test::TestDatabaseWrapper;
+    use crate::users::postgres::testdata;
     use spectral::prelude::*;
     use test_env_log::test;
+    use uuid::Uuid;
+    #[test]
+    fn test_find_unknown_user_by_id() {
+        let database = TestDatabaseWrapper::new();
+        let repository = PostgresUserRepository::new(database.wrapper.clone());
+
+        let user = repository.get_user_by_id(UserID::from_uuid(Uuid::new_v4()));
+        assert_that(&user).is_none();
+    }
+    #[test]
+    fn test_find_unknown_user_by_email() {
+        let database = TestDatabaseWrapper::new();
+        let repository = PostgresUserRepository::new(database.wrapper.clone());
+
+        let user = repository.get_user_by_email("testuser@example.com".to_owned());
+        assert_that(&user).is_none();
+    }
     #[test]
     fn test_find_unknown_user_by_username() {
         let database = TestDatabaseWrapper::new();
@@ -69,15 +94,43 @@ mod tests {
     }
 
     #[test]
-    fn test_find_known_user_by_username() {
-        let testuser = crate::users::postgres::testdata::User::default();
+    fn test_find_known_user_by_id() {
+        let testuser = testdata::User::default();
         let database = TestDatabaseWrapper::new();
 
         database.seed(vec![&testuser]);
 
         let repository = PostgresUserRepository::new(database.wrapper.clone());
 
-        let user = repository.get_user_by_username("testuser".parse().unwrap());
+        let user = repository.get_user_by_id(testuser.user_id.clone());
+        assert_user(testuser, user);
+    }
+    #[test]
+    fn test_find_known_user_by_email() {
+        let testuser = testdata::User::default();
+        let database = TestDatabaseWrapper::new();
+
+        database.seed(vec![&testuser]);
+
+        let repository = PostgresUserRepository::new(database.wrapper.clone());
+
+        let user = repository.get_user_by_email(testuser.email.clone());
+        assert_user(testuser, user);
+    }
+    #[test]
+    fn test_find_known_user_by_username() {
+        let testuser = testdata::User::default();
+        let database = TestDatabaseWrapper::new();
+
+        database.seed(vec![&testuser]);
+
+        let repository = PostgresUserRepository::new(database.wrapper.clone());
+
+        let user = repository.get_user_by_username(testuser.username.clone());
+        assert_user(testuser, user);
+    }
+
+    fn assert_user(testuser: testdata::User, user: Option<UserEntity>) {
         assert_that(&user).is_some().is_equal_to(UserEntity {
             identity: Identity {
                 id: testuser.user_id,
--- src/users/service/repository.rs
+++ src/users/service/repository.rs
@@ -1,13 +1,31 @@
-use crate::users::{UserEntity, Username};
+use crate::users::{UserEntity, UserID, Username};
 
 /// Trait defining the ways that we can interact with user entities in the database
 pub trait UserRepository {
+    /// Find the user that has the given ID
+    ///
+    /// # Arguments
+    /// * `user_id` The user ID
+    ///
+    /// # Returns
+    /// The user, if found. None if the user doesn't exist
+    fn get_user_by_id(&self, user_id: UserID) -> Option<UserEntity>;
+
     /// Find the user that has the given username
     ///
     /// # Arguments
-    /// * `username` The user with the username
+    /// * `username` The username
     ///
     /// # Returns
     /// The user, if found. None if the user doesn't exist
     fn get_user_by_username(&self, username: Username) -> Option<UserEntity>;
+
+    /// Find the user that has the given email address
+    ///
+    /// # Arguments
+    /// * `email` The email address
+    ///
+    /// # Returns
+    /// The user, if found. None if the user doesn't exist
+    fn get_user_by_email(&self, email: String) -> Option<UserEntity>;
 }
