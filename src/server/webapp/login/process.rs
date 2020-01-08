d78a68dc8036e0228a4adfeeaae1b116cddeec18
Graham Cox <graham@grahamcox.co.uk>
2020-01-08 09:42:34 +0000

    Parsing the login form into a Username now returns a map of errors

--- src/server/webapp/login/process.rs
+++ src/server/webapp/login/process.rs
@@ -11,12 +11,17 @@ pub struct LoginForm {
 }
 
 impl TryFrom<LoginForm> for Username {
-    type Error = String;
+    type Error = HashMap<&'static str, &'static str>;
     fn try_from(value: LoginForm) -> Result<Self, Self::Error> {
         value
             .username
-            .ok_or("missing".to_owned())
-            .and_then(|u| u.parse().map_err(|_| "missing".to_owned()))
+            .ok_or("missing")
+            .and_then(|u| u.parse().map_err(|_| "missing"))
+            .map_err(|e| {
+                let mut errors = HashMap::new();
+                errors.insert("username", e);
+                errors
+            })
     }
 }
 
@@ -24,15 +29,11 @@ impl TryFrom<LoginForm> for Username {
 #[tracing::instrument]
 pub fn process_login(form: LenientForm<LoginForm>, _request_id: RequestId) -> Template {
     let login_form = form.into_inner();
-    let username: Result<Username, String> = login_form.try_into();
+    let username: Result<Username, HashMap<&'static str, &'static str>> = login_form.try_into();
     log::debug!("Username parsed from form: {:?}", username);
 
     match username {
-        Err(e) => {
-            let mut errors = HashMap::new();
-            errors.insert("username", e);
-            Template::new("login/start.tera").with_data("errors", &errors)
-        }
+        Err(e) => Template::new("login/start.tera").with_data("errors", &e),
         Ok(username) => Template::new("login/register.tera").with_data("username", &username),
     }
 }
