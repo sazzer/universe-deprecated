0fac2cf41d7b2a60aa876556b1c743419135fb03
Graham Cox <graham@grahamcox.co.uk>
2020-01-24 18:34:53 +0000

    A bit of moving around

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,6 +2,6 @@ import './i18n';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App } from './ui/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
