974035f9fdd8bf2dc1731a3aa1d1dc4dbc6fa414
Graham Cox <graham@grahamcox.co.uk>
2020-01-24 18:14:45 +0000

    Set up i18n

--- webapp/src/header/index.tsx
+++ webapp/src/header/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
 
 export const Header: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">Universe</a>
+      <a className="navbar-brand" href="/">{t('page.title')}</a>
       <button className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
-        aria-label="Toggle Navigation">
+        aria-label="{t('header.toggleNavigation')}">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/login">Login / Register</a>
+            <a className="nav-link" href="/login">{t('header.loginMenu.title')}</a>
           </li>
         </ul>
       </div>
