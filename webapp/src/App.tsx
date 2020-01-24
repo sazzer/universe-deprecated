8f42d91f9c9ea225f63b498d41c276243a2cfb38
Graham Cox <graham@grahamcox.co.uk>
2020-01-24 18:18:38 +0000

    Set up URL Routing

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './header';
 
 export const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <Header />
+
       <div className="container-fluid mt-3">
+        <Switch>
+          <Route path='/login'>Login Page</Route>
+          <Route path='/'>Home Page</Route>
+        </Switch>
       </div>
-    </>
+    </Router>
   );
 }
