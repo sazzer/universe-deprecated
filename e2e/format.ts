8ad0a129bf57207e6f8ca158f1447bfefdafaa6f
Graham Cox <graham@grahamcox.co.uk>
2020-02-14 19:17:11 +0000

    Change the formatter of the E2E output

--- e2e/format.ts
+++ e2e/format.ts
@@ -1,15 +1,15 @@
-import * as reporter from 'cucumber-html-reporter';
+import * as reporter from "cucumber-html-reporter";
 
 var options = {
-  theme: 'foundation',
-  jsonFile: 'output/cucumber_report.json',
-  output: 'output/cucumber_report.html',
+  theme: "bootstrap",
+  jsonFile: "output/cucumber_report.json",
+  output: "output/cucumber_report.html",
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
   launchReport: false,
   storeScreenshots: true,
-  screenshotsDirectory: 'output/screenshots',
-  noInlineScreenshots: true,
+  screenshotsDirectory: "output/screenshots",
+  noInlineScreenshots: true
 };
 
 reporter.generate(options);
