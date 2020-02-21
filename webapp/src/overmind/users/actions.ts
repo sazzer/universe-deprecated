e9dfec825f3a4df5cc3b21ef741711cdf3c4736d
Graham Cox <graham@grahamcox.co.uk>
2020-02-21 16:08:58 +0000

    Lint fix

--- webapp/src/overmind/users/actions.ts
+++ webapp/src/overmind/users/actions.ts
@@ -5,7 +5,9 @@ import { User } from "./state";
  * Overmind action for storing the details of a user
  */
 export const storeUser: Action<User> = ({ state }, details: User) => {
-  const users = state.users.users.filter(user => user.userId != details.userId);
+  const users = state.users.users.filter(
+    user => user.userId !== details.userId
+  );
   users.push({
     userId: details.userId,
     username: details.username,
