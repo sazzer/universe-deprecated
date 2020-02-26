538531715ff736e690d70455c7dcaea942295f13
Graham Cox <graham@grahamcox.co.uk>
2020-02-26 10:46:05 +0000

    Missing braces around translation call

--- webapp/src/ui/loader.tsx
+++ webapp/src/ui/loader.tsx
@@ -10,7 +10,7 @@ export const Loader: React.FC = () => {
   return (
     <div className="d-flex justify-content-center">
       <div className="spinner-border" role="status">
-        <span className="sr-only">t('loader.label')</span>
+        <span className="sr-only">{t("loader.label")}</span>
       </div>
     </div>
   );
