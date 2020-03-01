d93f81a3a3714fea14ae34bd778d11d9ed3780dd
Graham Cox <graham@grahamcox.co.uk>
2020-03-01 17:42:10 +0000

    Render the home page

--- webapp/src/ui/App.tsx
+++ webapp/src/ui/App.tsx
@@ -1,6 +1,8 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
 import { HeaderBar } from "./header";
+import { HomePage } from "./homePage";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 /**
  * The main entrypoint into the application
@@ -9,6 +11,12 @@ export const App: React.FC = () => {
   return (
     <Router>
       <HeaderBar />
+
+      <Switch>
+        <Route>
+          <HomePage />
+        </Route>
+      </Switch>
     </Router>
   );
 };
---
+++ webapp/src/ui/homePage.tsx
@@ -0,0 +1,19 @@
+import { LandingPage } from "./landingPage";
+import React from "react";
+
+/**
+ * Component to represent the home page, if nothing else is seen
+ */
+export const HomePage: React.FC = () => {
+  return (
+    <LandingPage>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </LandingPage>
+  );
+};
