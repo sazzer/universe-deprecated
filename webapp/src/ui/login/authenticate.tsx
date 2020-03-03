d031b7fa3b86d66f823f2be00c19c805caee08bf
Graham Cox <graham@grahamcox.co.uk>
2020-03-03 07:46:53 +0000

    Form for authenticating an existing user

--- webapp/src/ui/login/authenticate.tsx
+++ webapp/src/ui/login/authenticate.tsx
@@ -1,4 +1,14 @@
-import React from "react";
+import * as yup from "yup";
+
+import { ErrorMessage, FieldValues, useForm } from "react-hook-form";
+import React, { useState } from "react";
+
+import { ValidationErrors } from "../../api/validation";
+import debug from "debug";
+import { useTranslation } from "react-i18next";
+
+/** The logger to use */
+const LOG = debug("universe:ui:login:authenticate");
 
 /**
  * Props for the Authenticate User page
@@ -15,5 +25,119 @@ export const AuthenticateUserPage: React.FC<AuthenticateUserPageProps> = ({
   username,
   onCancel
 }) => {
-  return <>{username}</>;
+  const { t } = useTranslation();
+  const [loading, setLoading] = useState(false);
+  const [globalError, setGlobalError] = useState<String | undefined>(undefined);
+
+  const { register, errors, handleSubmit, setError } = useForm({
+    validationSchema: yup.object().shape({
+      username: yup
+        .string()
+        .required(
+          t("login.username.errors.tag:universe,2020:validation-errors/missing")
+        )
+        .trim(),
+      password: yup
+        .string()
+        .required(
+          t("login.password.errors.tag:universe,2020:validation-errors/missing")
+        )
+    }),
+    validateCriteriaMode: "all",
+    defaultValues: {
+      username: username,
+      password: ""
+    }
+  });
+
+  const onSubmitHandler = async (data: FieldValues) => {
+    LOG("Submitting form: %o", data);
+    setGlobalError(undefined);
+    setLoading(true);
+  };
+
+  let errorMessage;
+  if (globalError) {
+    errorMessage = (
+      <div className="form-group">
+        <div className="alert alert-danger" role="alert">
+          {t("errors.unexpected", {
+            message: globalError
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h3>{t("login.authenticate.title")}</h3>
+      <form
+        onSubmit={handleSubmit(onSubmitHandler)}
+        data-test="authenticateForm"
+      >
+        <div className="form-group" data-test="username">
+          <label htmlFor="username">{t("login.username.label")}</label>
+          <input
+            type="text"
+            className={
+              errors.username ? "form-control is-invalid" : "form-control"
+            }
+            id="username"
+            name="username"
+            readOnly
+            ref={register}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="username"
+            as={<div className="invalid-feedback" />}
+          />
+        </div>
+        <div className="form-group" data-test="password">
+          <label htmlFor="password">{t("login.password.label")}</label>
+          <input
+            type="password"
+            className={
+              errors.password ? "form-control is-invalid" : "form-control"
+            }
+            id="password"
+            name="password"
+            autoFocus
+            readOnly={loading}
+            ref={register}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            as={<div className="invalid-feedback" />}
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary" disabled={loading}>
+            {loading && (
+              <>
+                <span
+                  className="spinner-border spinner-border-sm"
+                  role="status"
+                  aria-hidden="true"
+                ></span>
+                &nbsp;
+              </>
+            )}
+            {t("login.authenticate.submit")}
+          </button>
+          <button
+            type="button"
+            className="btn btn-link"
+            disabled={loading}
+            onClick={onCancel}
+          >
+            {t("login.authenticate.cancel")}
+          </button>
+        </div>
+        {errorMessage}
+      </form>
+    </>
+  );
 };
