4b2ac66009850f0ac783bab3679c47c1bbb38e35
Graham Cox <graham@grahamcox.co.uk>
2020-03-21 11:27:03 +0000

    Logging and configuration

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+import "dotenv/config";
+
+import config from "config";
+import pino from "pino";
+
+/** The logger to use */
+const LOG = pino({
+  name: "universe",
+  prettyPrint: process.env.NODE_ENV === "development"
+});
+
+LOG.info("Config", config);
